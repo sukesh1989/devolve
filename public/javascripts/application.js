433876b14dc95804d0f78b6d4157c6cbdcc3fe14
sukesh salian <devolve.india@gmail.com>
2011-03-20 21:26:50 +0530

    after login

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,16 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+
+$(function() {
+ $("form").submit(function(){
+    $(this).fadeOut(200, function(){
+      $('#logoname').animate({
+        'margin-top': '+=80',
+ 		'margin-left': '+=10'
+      }, 250, function(){
+        $("#spinner").fadeIn(300);
+      });
+    });
+  });
+});

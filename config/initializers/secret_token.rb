7aabf004537c8d0f2a799504debee099ddecc195
sukesh salian <devolve.india@gmail.com>
2011-03-20 17:15:30 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Devolve::Application.config.secret_token = 'ca1d39e2999012ab4ae877fa6390efeb86da1a87c256d501517b38cfc44112e5a67378a966650137f52d29bff584f4936bde64f18e0288af12d37ea405b03aad'

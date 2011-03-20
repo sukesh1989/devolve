3aaa007e3c862166c585e7979b76c72609181e39
sukesh salian <devolve.india@gmail.com>
2011-03-20 17:39:47 +0530

    third

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   def index
+      if user_signed_in?
+        render :index
+      else  
+          redirect_to new_user_session_path
+    end
+
   end
 
 end

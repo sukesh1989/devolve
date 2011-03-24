bb2432c2927b963122a093609eee83c5fb8d3e1d
sukesh salian <devolve.india@gmail.com>
2011-03-24 19:41:12 +0530

    anima

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,14 +2,18 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 
-$(document).ready( function(){
-   $("form").submit(function(){
-     $("#html_new").fadeOut(200, function(){
-       $('#logoname1').fadeIn({
-         'margin-top': '+=80'
-       }, 250, function(){
-         $("#spinner").fadeIn(300);
-       });
-     });
-   });
- });
+$(function() {
+ 
+      $("form").submit(function(){
+        $(this).fadeOut(200, function(){
+          $('#logoname1').fadeIn({
+            'margin-top': '+=80'
+          }, 250, function(){
+            $("#spinner").fadeIn(300);
+         
+        });
+      });
+    });
+
+
+});

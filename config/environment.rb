7f79bd4eb9cc1fced76a63e036673bec458eb3b6
sukesh salian <devolve.india@gmail.com>
2011-03-27 18:18:42 +0530

    signup_completed_done

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,4 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Devolve::Application.initialize!
+ActionView::Base.field_error_proc = proc { |input, instance| input } 

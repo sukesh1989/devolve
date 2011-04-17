7570e3eea7a7643228edcef863634d389c4b817a
sukesh salian <devolve.india@gmail.com>
2011-04-17 19:07:06 +0530

    push_after_syntax

--- Gemfile
+++ Gemfile
@@ -15,7 +15,8 @@ gem 'will_paginate', '3.0.pre2'
 
 # Use unicorn as the web server
 # gem 'unicorn'
-
+gem 'coderay'
+gem 'RedCloth'
 
 # Deploy with Capistrano
 # gem 'capistrano'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  
+  def coderay(text)
+    text.gsub!(/\<code(?: lang="(.+?)")?\>(.+?)\<\/code\>/m) do 
+         code = CodeRay.scan($2, $1).div(:css => :class) 
+         "<notextile>#{code}</notextile>" 
+       end 
+       return text.html_safe
+  end
+  
+ 
 end
